ab22eb93273f73e361946639e3afa6be6531edab
dennis.hartrampf <dennis.hartrampf@web.de>
2019-02-04 14:11:41 +0100

    Get rid of unnecessary map of Signals

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,12 +1,9 @@
-import Signal.*
 import kotlin.math.pow
 
 object HandshakeCalculator {
 
-    private val actions = mapOf(0 to WINK, 1 to DOUBLE_BLINK, 2 to CLOSE_YOUR_EYES, 3 to JUMP)
-
     fun calculateHandshake(input: Int): List<Signal> {
-        return actions.filterKeys { input isDigitSet it }.values.toList().let { if (input isDigitSet 4) it.reversed() else it }
+        return Signal.values().filterIndexed { i, _ -> input isDigitSet i }.let { if (input isDigitSet 4) it.reversed() else it }
     }
 
     private infix fun Int.isDigitSet(digit: Int): Boolean = 2.pow(digit).let { this and it == it }
