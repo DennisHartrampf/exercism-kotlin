17427fe7f6f5e7cce9bdc10db8f81bc8c4edd462
dennis.hartrampf <dennis.hartrampf@web.de>
2019-03-01 14:35:57 +0100

    Remove explicit return type

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -9,7 +9,7 @@ object WordCount {
 
     private fun String.toWords() = split(splitPattern).asSequence()
 
-    private fun <T> Sequence<T>.toMapCounting(): Map<T, Int> = groupBy { it }.mapValues { it.value.size }
+    private fun <T> Sequence<T>.toMapCounting() = groupBy { it }.mapValues { it.value.size }
 
     private fun Sequence<String>.normalize() =
             filter { !it.isBlank() }.map(String::toLowerCase).map {
