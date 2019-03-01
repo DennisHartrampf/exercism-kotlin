2da400ca15c134545d356cfcd3060c0930fdde19
dennis.hartrampf <dennis.hartrampf@web.de>
2019-03-01 14:33:40 +0100

    Use groupBy and mapValues

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -7,13 +7,12 @@ object WordCount {
 
     fun phrase(input: String) = input.toWords().normalize().toMapCounting()
 
-    private fun String.toWords() = splitPattern.split(this).asSequence()
+    private fun String.toWords() = split(splitPattern).asSequence()
 
-    private fun <T> Sequence<T>.toMapCounting(): Map<T, Int> =
-            this.map { it to 1 }.fold(mutableMapOf()) { map, pair -> map.merge(pair.first, 1) { t, u -> t + u }; map }
+    private fun <T> Sequence<T>.toMapCounting(): Map<T, Int> = groupBy { it }.mapValues { it.value.size }
 
     private fun Sequence<String>.normalize() =
-            this.filter { !it.isBlank() }.map(String::toLowerCase).map {
+            filter { !it.isBlank() }.map(String::toLowerCase).map {
                 val matcher = quotePattern.matcher(it)
                 if (matcher.matches()) matcher.group(1)
                 else it
