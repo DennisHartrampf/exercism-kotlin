56f7c31794406e5727a7348295879e8fa8369192
dennis.hartrampf <dennis.hartrampf@web.de>
2019-03-05 12:36:30 +0100

    Forth interpreter - refactor operations

--- forth/src/main/kotlin/ForthEvaluator.kt
+++ forth/src/main/kotlin/ForthEvaluator.kt
@@ -4,16 +4,14 @@ class ForthEvaluator {
     private val stack = LinkedList<Int>()
 
     fun evaluateProgram(instructions: List<String>): List<Int> {
-        instructions.flatMap { it.split(" ") }.map { tokenize(it) }.forEach(this@ForthEvaluator::evaluate)
+        instructions.flatMap { it.split(" ") }.map { tokenize(it) }.forEach { evaluate(it) }
         return stack.reversed()
     }
 
     private fun tokenize(word: String): Any {
-        val binaryOperation = BinaryOperations.of(word)
-        val stackOperation = StackOperations.of(word)
+        val operation = Operations.of(word)
         return when {
-            binaryOperation != null -> binaryOperation
-            stackOperation != null -> stackOperation
+            operation != null -> operation
             NUMBER.matches(word) -> word.toInt()
             else -> throw IllegalArgumentException("Unknown word: " + word)
         }
@@ -22,74 +20,11 @@ class ForthEvaluator {
     private fun evaluate(token: Any) {
         when (token) {
             is Int -> stack.push(token)
-            is BinaryOperations -> performBinaryOperation(token)
-            is StackOperations -> performStackOperation(token)
+            is Operations -> token.performOperation(stack)
         }
-    }
-
-    private fun performBinaryOperation(token: BinaryOperations) {
-        if (stack.size < 2) {
-            throw IllegalArgumentException("${token.spokenName} requires that the stack contain at least 2 values")
-        }
-        val secondOperand = stack.pop()
-        val firstOperand = stack.pop()
-        stack.push(token.operation.invoke(firstOperand, secondOperand))
-    }
-
-    private fun performStackOperation(token: StackOperations) {
-        if (stack.size < token.requiredOperands) {
-            throw IllegalArgumentException("${token.spokenName} requires that the stack contain at least " +
-                    "${token.requiredOperands} ${if (token.requiredOperands == 1) "value" else "values"}")
-        }
-        token.operation.invoke(stack)
     }
 
     companion object {
         val NUMBER = Regex("[0-9]+")
-    }
-}
-
-enum class BinaryOperations(val token: kotlin.String, val spokenName: kotlin.String, val operation: (Int, Int) -> Int) {
-    PLUS("+", "Addition", { o1, o2 -> o1 + o2 }),
-    MINUS("-", "Subtraction", { o1, o2 -> o1 - o2 }),
-    MULTIPLICATION("*", "Multiplication", { o1, o2 -> o1 * o2 }),
-    DIVISION("/", "Division", { o1, o2 ->
-        if (o2 == 0) {
-            throw IllegalArgumentException("Division by 0 is not allowed")
-        } else {
-            o1 / o2
-        }
-    }),
-    ;
-
-    companion object {
-        private val map = values().map { Pair(it.token, it) }.toMap()
-
-        fun of(word: String): BinaryOperations? {
-            return map[word]
-        }
-    }
-}
-
-enum class StackOperations(val token: kotlin.String, val spokenName: kotlin.String, val requiredOperands: Int, val operation: (LinkedList<Int>) -> Unit) {
-    DUPLICATE("dup", "Duplicating", 1, { it.push(it.peek()) }),
-    DROP("drop", "Dropping", 1, { it.pop() }),
-    SWAP("swap", "Swapping", 2, {
-        val first = it.pop();
-        val second = it.pop()
-        it.push(first)
-        it.push(second)
-    }),
-    OVER("over", "Overing", 2, {
-        it.push(it[1])
-    }),
-    ;
-
-    companion object {
-        private val map = values().map { Pair(it.token.toLowerCase(), it) }.toMap()
-
-        fun of(word: String): StackOperations? {
-            return map[word.toLowerCase()]
-        }
     }
 }
---
+++ forth/src/main/kotlin/Operations.kt
@@ -0,0 +1,51 @@
+import java.util.*
+
+enum class Operations(val token: kotlin.String, private val spokenName: kotlin.String, private val requiredOperands: Int, private val operation: (LinkedList<Int>) -> Unit) {
+    PLUS("+", "Addition", 2, { it performBinaryOperation { o1, o2 -> o1 + o2 } }),
+    MINUS("-", "Subtraction", 2, { it performBinaryOperation { o1, o2 -> o1 - o2 } }),
+    MULTIPLICATION("*", "Multiplication", 2, { it performBinaryOperation { o1, o2 -> o1 * o2 } }),
+    DIVISION("/", "Division", 2, {
+        it performBinaryOperation { o1, o2 ->
+            if (o2 == 0) {
+                throw IllegalArgumentException("Division by 0 is not allowed")
+            } else {
+                o1 / o2
+            }
+        }
+    }),
+
+    DUPLICATE("dup", "Duplicating", 1, { it.push(it.peek()) }),
+    DROP("drop", "Dropping", 1, { it.pop() }),
+    SWAP("swap", "Swapping", 2, {
+        val first = it.pop()
+        val second = it.pop()
+        it.push(first)
+        it.push(second)
+    }),
+    OVER("over", "Overing", 2, {
+        it.push(it[1])
+    }),
+    ;
+
+    fun performOperation(stack: LinkedList<Int>) {
+        if (stack.size < requiredOperands) {
+            throw IllegalArgumentException("$spokenName requires that the stack contain at least " +
+                    "$requiredOperands ${if (requiredOperands == 1) "value" else "values"}")
+        }
+        operation.invoke(stack)
+    }
+
+    companion object {
+        private val map = values().map { Pair(it.token.toLowerCase(), it) }.toMap()
+
+        fun of(word: String): Operations? {
+            return map[word.toLowerCase()]
+        }
+
+        private infix fun LinkedList<Int>.performBinaryOperation(binaryOperation: (Int, Int) -> Int) {
+            val secondOperand = this.pop()
+            val firstOperand = this.pop()
+            this.push(binaryOperation.invoke(firstOperand, secondOperand))
+        }
+    }
+}
