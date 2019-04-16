f5188b758fe18f7eb810703d8bd06dcc68aeee80
dennis.hartrampf <dennis.hartrampf@web.de>
2019-04-16 11:27:07 +0200

    Don't use flatMap

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,9 +1,15 @@
 object Flattener {
 
-    fun flatten(any: Any?): List<Any> = when {
-        any is Iterable<*> -> any.flatMap { flatten(it) }
-        any != null -> listOf(any)
-        else -> listOf()
+    fun flatten(input: Any?): List<Any> {
+        return flatten(mutableListOf(), input)
+    }
+
+    private fun flatten(flatList: MutableList<Any>, any: Any?): List<Any> {
+        when {
+            any is Iterable<*> -> any.forEach { flatten(flatList, it) }
+            any != null -> flatList.add(any)
+        }
+        return flatList
     }
 
 }
