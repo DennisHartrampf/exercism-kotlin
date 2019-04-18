b619aaaab3c1c06a31850db16f5fb1b480a39ca8
dennis.hartrampf <dennis.hartrampf@web.de>
2019-04-18 14:51:40 +0200

    Use different approach for determining triangle properties

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -3,26 +3,20 @@ class Triangle(sideA: Double, sideB: Double, sideC: Double) {
     constructor(sideA: Number, sideB: Number, sideC: Number) : this(sideA.toDouble(), sideB.toDouble(), sideC.toDouble())
 
     init {
-        if (!isTriangle(sideA, sideB, sideC)) {
-            throw IllegalArgumentException()
-        }
+        require(isTriangle(sideA, sideB, sideC))
     }
 
-    val isEquilateral = sideA == sideB && sideA == sideC
-    val isIsosceles = sideA == sideB || sideA == sideC || sideB == sideC
-    val isScalene = !isIsosceles
+    private val numberOfUniqueSides = setOf(sideA, sideB, sideC).size
+    val isEquilateral = numberOfUniqueSides == 1
+    val isIsosceles = numberOfUniqueSides <= 2
+    val isScalene = numberOfUniqueSides == 3
 
     private fun isTriangle(sideA: Double, sideB: Double, sideC: Double) =
-            allSidesGreaterZero(sideA, sideB, sideC) &&
-                    allSidesSatisfyTriangleInequality(sideA, sideB, sideC)
+            allSidesGreaterZero(sideA, sideB, sideC) && satisfiesTriangleInequality(sideA, sideB, sideC)
 
     private fun allSidesGreaterZero(sideA: Double, sideB: Double, sideC: Double) = sideA > 0 && sideB > 0 && sideC > 0
 
-    private fun allSidesSatisfyTriangleInequality(sideA: Double, sideB: Double, sideC: Double) =
-            satisfiesTriangleInequality(sideA, sideB, sideC) &&
-                    satisfiesTriangleInequality(sideA, sideC, sideB) &&
-                    satisfiesTriangleInequality(sideB, sideC, sideA)
-
-    private fun satisfiesTriangleInequality(side1: Double, side2: Double, side3: Double) = side1 + side2 >= side3
+    private fun satisfiesTriangleInequality(sideA: Double, sideB: Double, sideC: Double) =
+            2 * setOf(sideA, sideB, sideC).max()!! < sideA + sideB + sideC
 
 }
