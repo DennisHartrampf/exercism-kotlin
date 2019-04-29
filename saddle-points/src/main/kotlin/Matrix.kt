38991c39680e5685dad4ec3d75bd74dd29f01f60
dennis.hartrampf <dennis.hartrampf@web.de>
2019-04-29 10:48:23 +0200

    Made long expressions easier to read by breaking them down

--- saddle-points/src/main/kotlin/Matrix.kt
+++ saddle-points/src/main/kotlin/Matrix.kt
@@ -1,15 +1,21 @@
 internal class Matrix(matrix: MyMatrix) {
 
-    val saddlePoints = matrix.coordinates().filter { (y, x) -> matrix.hasSaddlePointAt(y, x) }.toCoordinates()
+    val saddlePoints = matrix.coordinates()
+            .filter { (y, x) -> matrix.hasSaddlePointAt(y, x) }.toCoordinates()
 
-    private fun MyMatrix.coordinates() = mapIndexed { y, row -> row.indices.map { x -> y to x } }.flatten()
+    private fun MyMatrix.coordinates(): List<Pair<Int, Int>> {
+        return mapIndexed { y, row -> row.indices.map { x -> y to x } }.flatten()
+    }
 
     private fun MyMatrix.hasSaddlePointAt(row: Int, column: Int): Boolean {
         val value = this[row][column]
-        return this[row].all { value >= it } && this.map { it[column] }.all { value <= it }
+        val columns = this.map { it[column] }
+        return this[row].all { value >= it } && columns.all { value <= it }
     }
 
-    private fun List<Pair<Int, Int>>.toCoordinates() = map { (y, x) -> MatrixCoordinate(y, x) }.toSet()
+    private fun List<Pair<Int, Int>>.toCoordinates(): Set<MatrixCoordinate> {
+        return map { (y, x) -> MatrixCoordinate(y, x) }.toSet()
+    }
 
 }
 
