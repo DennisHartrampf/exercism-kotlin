1fb8304767fdbda4b7b6af442cb92914a68905e2
Dennis Hartrampf <d.hartrampf@bvv.de>
2022-03-31 09:25:31 +0200

    Diamond - non-recursive functional style

--- diamond/src/main/kotlin/DiamondPrinter.kt
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,21 +1,14 @@
 class DiamondPrinter {
-    fun printToList(char: Char): List<String> = char.toDiamond()
-
-    private fun Char.toDiamond(): List<String> = toHalfDiamond(2 * (this - START_CHAR) + 1, 0, listOf()).mirrored()
-
-    private tailrec fun Char.toHalfDiamond(size: Int, offset: Int, diamond: List<String>): List<String> {
-        val newDiamond = diamond + this.toDiamondLine(size, offset)
-        return if (this == START_CHAR) {
-            newDiamond
-        } else {
-            (this - 1).toHalfDiamond(size, offset + 1, newDiamond)
-        }
+    fun printToList(char: Char): List<String> {
+        val size = 2 * (char - START_CHAR) + 1
+        val lowerHalfOfDiamond = char.downTo(START_CHAR).mapIndexed { offset, it -> it.toDiamondLine(size, offset) }
+        return lowerHalfOfDiamond.mirrored()
     }
 
     private fun Char.toDiamondLine(size: Int, offset: Int): String =
-        CharArray(size) { ' ' }.apply {
-            this[offset] = this@toDiamondLine
-            this[size - offset - 1] = this@toDiamondLine
+        CharArray(size) { ' ' }.also {
+            it[offset] = this
+            it[size - offset - 1] = this
         }.joinToString(separator = "")
 
     private fun <T> List<T>.mirrored(): List<T> = if (size <= 1) {
