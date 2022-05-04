a5b8e203750a097b10bb5ec94306f0f090d594d2
Dennis Hartrampf <d.hartrampf@bvv.de>
2022-05-04 10:37:21 +0200

    Matching Brackets: Refactor

--- matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -4,19 +4,23 @@ object MatchingBrackets {
     private val CLOSING_BRACKETS = setOf(')', ']', '}')
     private val MATCHING_BRACKETS = CLOSING_BRACKETS.zip(OPENING_BRACKETS).toMap()
 
-    fun isValid(input: String): Boolean = input.fold(mutableListOf<Char>()) { stack, char ->
-        when (char) {
-            in OPENING_BRACKETS -> {
-                stack.add(char)
-            }
-            in CLOSING_BRACKETS -> {
-                val openingBracket = stack.removeLastOrNull()
-                if (MATCHING_BRACKETS[char] != openingBracket) {
-                    return false
-                }
-            }
-        }
-        stack
-    }.isEmpty()
+    fun isValid(input: String): Boolean = mutableListOf<Char>().let { brackets -> input.all { brackets.isValid(it) } && brackets.isEmpty() }
 
+    private fun MutableList<Char>.isValid(char: Char): Boolean = when (char) {
+        in OPENING_BRACKETS -> {
+            openBracket(char)
+        }
+        in CLOSING_BRACKETS -> {
+            closeBracket(char)
+        }
+        else -> true
+    }
+
+    private fun MutableList<Char>.openBracket(openingBracket: Char): Boolean {
+        add(openingBracket)
+        return true
+    }
+
+    private fun MutableList<Char>.closeBracket(closingBracket: Char): Boolean =
+        removeLastOrNull().let { openingBracket -> MATCHING_BRACKETS[closingBracket] == openingBracket }
 }
