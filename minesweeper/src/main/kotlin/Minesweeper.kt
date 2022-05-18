03220ff28c5cd671a506dd4d4f1c2679f8567a43
Dennis Hartrampf <d.hartrampf@bvv.de>
2022-05-18 16:37:13 +0200

    Minesweeper: Replace Field classes by when-expression

--- minesweeper/src/main/kotlin/Minesweeper.kt
+++ minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,6 +1,6 @@
 data class MinesweeperBoard(val board: List<String>) {
 
-    private val numberedBoard: List<List<Field>>
+    private val numberedBoard: List<String>
     private val width = board.firstOrNull()?.length ?: 0
     private val height = board.size
 
@@ -8,25 +8,28 @@ data class MinesweeperBoard(val board: List<String>) {
         numberedBoard = board.toNumberedBoard()
     }
 
-    private fun List<String>.toNumberedBoard() = toFields().apply {
+    fun withNumbers(): List<String> = numberedBoard
+
+    private fun List<String>.toNumberedBoard() = toMutableList().apply {
         forEachIndexed { y, line ->
             line.forEachIndexed { x, currentField ->
-                if (currentField is MineField) {
+                if (currentField == MINE_CHAR) {
                     increaseMineCountOfAdjacentFields(x, y)
                 }
             }
         }
     }
 
-    private fun List<String>.toFields() = mapIndexed { y, line -> line.toFields(y).toMutableList() }.toMutableList()
+    private fun MutableList<String>.increaseMineCountOfAdjacentFields(mineX: Int, mineY: Int) =
+        getAdjacentFields(mineX, mineY).forEach { (x, y) -> this[y] = this[y].incremented(x) }
 
-    private fun String.toFields(y: Int) = mapIndexed { x, char -> if (char == MINE_CHAR) MineField(x, y) else SafeField(x, y) }
+    private fun String.incremented(x: Int) = StringBuilder(this).apply { setCharAt(x, this[x].incremented()) }.toString()
 
-    private fun MutableList<MutableList<Field>>.increaseMineCountOfAdjacentFields(x: Int, y: Int) =
-        getAdjacentFields(x, y).map { (x, y) -> this[y][x] }.filterIsInstance<SafeField>()
-            .forEach { field -> this[field.y][field.x] = field.incremented() }
-
-    fun withNumbers(): List<String> = numberedBoard.map { line -> line.map { it.display() }.joinToString(separator = "") }
+    private fun Char.incremented() = when (this) {
+        MINE_CHAR -> MINE_CHAR
+        EMPTY_FIELD_CHAR -> '1'
+        else -> this + 1
+    }
 
     private fun getAdjacentFields(x: Int, y: Int): List<Pair<Int, Int>> = listOf(
         x - 1 to y - 1, x to y - 1, x + 1 to y - 1,
@@ -39,20 +42,4 @@ data class MinesweeperBoard(val board: List<String>) {
         private const val EMPTY_FIELD_CHAR = ' '
     }
 
-    private sealed class Field(val x: Int, val y: Int) {
-        open fun incremented() = this
-        abstract fun display(): Char
-    }
-
-    private class MineField(x: Int, y: Int) : Field(x, y) {
-        override fun display() = MINE_CHAR
-    }
-
-    private class SafeField(x: Int, y: Int, private val minesCounter: Int = 0) : Field(x, y) {
-        override fun incremented(): Field {
-            return SafeField(x, y, minesCounter + 1)
-        }
-
-        override fun display() = if (minesCounter == 0) EMPTY_FIELD_CHAR else minesCounter.digitToChar()
-    }
 }
