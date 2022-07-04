2c8f90f8926f4b092247938fd88072c0ca4e6b8a
Dennis Hartrampf <d.hartrampf@bvv.de>
2022-07-04 11:36:29 +0200

    Algorithm complete. Needs refactoring and maybe short-circuit stop on first solution

--- dominoes/src/main/kotlin/Dominoes.kt
+++ dominoes/src/main/kotlin/Dominoes.kt
@@ -2,44 +2,45 @@ class ChainNotFoundException(msg: String) : RuntimeException(msg)
 
 private var NEXT_ID = 0
 
-class Domino(val left: Int, val right: Int) {
+class Domino(val left: Int, val right: Int, private val id: Int = NEXT_ID++) {
 
-    private val id = NEXT_ID++
+    override fun equals(other: Any?): Boolean = other is Domino && other.id == id
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+    override fun hashCode(): Int = id
 
-        other as Domino
+    override fun toString(): String = "Domino(left=$left, right=$right, id=$id)"
 
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
-
-    override fun toString(): String {
-        return "Domino(left=$left, right=$right, id=$id)"
-    }
-
+    fun flipped(): Domino = Domino(right, left, id)
 }
 
-private class Node(val domino: Domino, val remainingDominoes: Map<Int, Set<Domino>>) {
+private class Node(private val domino: Domino, remainingDominoes: Map<Int, Set<Domino>>, private val parent: Node?) {
+    val solutions = mutableListOf<List<Domino>>()
+
+    init {
+        if (remainingDominoes.values.all { it.isEmpty() }) {
+            registerSolution(listOf())
+        }
+    }
+
     val children: List<Node> = remainingDominoes[domino.right]?.map {
         val remainingDominoesChild = remainingDominoes.toMutableMap()
         remainingDominoesChild[it.left] = remainingDominoesChild[it.left].without(it)
         remainingDominoesChild[it.right] = remainingDominoesChild[it.right].without(it)
-        Node(it, remainingDominoesChild)
+        Node(it, remainingDominoesChild, this)
     } ?: emptyList()
 
     private fun Set<Domino>?.without(domino: Domino) = if (this == null) emptySet() else (this.toMutableSet() - domino)
 
-    override fun toString(): String {
-        return "Node(domino=$domino, children=$children, remainingDominoes=$remainingDominoes)"
+    private fun registerSolution(partialSolution: List<Domino>) {
+        val solution = listOf(this.domino) + partialSolution
+        if (parent == null) {
+            solutions.add(solution)
+        } else {
+            parent.registerSolution(solution)
+        }
     }
+
+    override fun toString(): String = "Node(domino=$domino, children=$children)"
 
 }
 
@@ -51,19 +52,14 @@ object Dominoes {
         }
         requirePreconditions(inputDominoes)
 
-        val firstDomino = inputDominoes.first()
+        val remainingDominoes = findRemainingDominoes(inputDominoes.drop(1))
+        val root = Node(inputDominoes.first(), remainingDominoes, null)
 
-        val theMap: Map<Int, Set<Domino>> = createMap(inputDominoes.drop(1))
-        val root = Node(firstDomino, theMap)
-
-        return mutableListOf(firstDomino)
+        return root.solutions.firstOrNull() ?: throw ChainNotFoundException("No chain found")
     }
 
-    private fun createMap(dominoes: List<Domino>): Map<Int, Set<Domino>> {
-        val map = mutableMapOf<Int, MutableSet<Domino>>()
-        dominoes.forEach { map.merge(it.left, mutableSetOf(it)) { a, b -> (a + b).toMutableSet() } }
-        return map
-    }
+    private fun findRemainingDominoes(dominoes: List<Domino>): Map<Int, Set<Domino>> =
+        (dominoes + dominoes.map { it.flipped() }).groupBy { it.left }.mapValues { it.value.toSet() }
 
     private fun requirePreconditions(inputDominoes: List<Domino>) {
         if (inputDominoes.flatMap { listOf(it.left, it.right) }.groupBy { it }.map { it.value.size }.any { it % 2 != 0 }) {
